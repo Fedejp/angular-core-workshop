186f303da5fe9e401d6412730e76eba056558d20
Fede Parodi <fedejp@Fedes-MacBook-Pro.local>
2019-12-02 17:18:29 -0300

    material lib done

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MaterialModule } from '@workshop/material';
+
 
 @NgModule({
   declarations: [AppComponent],
