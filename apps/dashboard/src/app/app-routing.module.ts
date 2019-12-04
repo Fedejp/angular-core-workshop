d250d38d1b341857b41c12cccc949c87bf56fc29
Fede Parodi <fedejp@Fedes-MacBook-Pro.local>
2019-12-04 15:31:27 -0300

    aca dejo de andar

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@workshop/ui-login';
 
 
+
 const routes: Routes = [
     { path:'', component : LoginComponent },
-    { path: '**', redirectTo:'', pathMatch: 'full'} //Si se trata de ir a una ruta que no existe, va al root por default
+    { path: '**', redirectTo: '' , pathMatch: 'full'} //Si se trata de ir a una ruta que no existe, va al root por default
 ];
 
 @NgModule({
